0a3f74cafe7216be150e7c0674f0a70bd8e17270
Vishal Palaniappan <101469936+vishalpalaniappan@users.noreply.github.com>
2025-01-22 14:40:27 -0500

    Add initial structure to accept and validate source file. (#1)

---
+++ adli.py
@@ -0,0 +1,28 @@
+import sys
+import argparse
+import processFile
+
+def main(argv):
+    args_parser = argparse.ArgumentParser(
+        description="Injects diagnostic logs into source file."
+    )
+
+    args_parser.add_argument(
+        "source",
+        type=str,
+        help="Path to source file"
+    )
+    
+    parsed_args = args_parser.parse_args(argv[1:])
+    source = parsed_args.source
+
+    try:
+        open(source)
+    except Exception as e:
+        print(f"Invalid arguments: {str(e)}", file=sys.stderr)
+        return -1
+
+    processFile(source)
+
+if "__main__" == __name__:
+    sys.exit(main(sys.argv))
