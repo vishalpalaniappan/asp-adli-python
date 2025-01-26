70fc057fc3a4cec94a9e93c47d6c71553d221184
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-01-26 15:31:34 -0500

    Add for to valid variable node types.

--- injector/NodeExtractor.py
+++ injector/NodeExtractor.py
@@ -11,7 +11,7 @@ class NodeExtractor():
         be extended to support variables.
     """
     def __init__(self, node):
-        self.VARIABLE_NODE_TYPES = (ast.Assign)
+        self.VARIABLE_NODE_TYPES = (ast.Assign, ast.For)
         self.lineno = node.lineno
         self.vars = []
 
