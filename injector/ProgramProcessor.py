5c58d28812b23417a9f336afa913a1ee51bf3a7b
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-02-03 14:16:12 -0500

    Refactor output folder and file creation.

--- injector/ProgramProcessor.py
+++ injector/ProgramProcessor.py
@@ -1,10 +1,6 @@
-from injector import helper
-import pathlib
 import shutil
-import json
-import ast
 import os
-from injector.LogInjector import LogInjector
+from injector.FindLocalImports import findLocalImports
 
 class ProgramProcessor:
     '''
@@ -13,96 +9,31 @@ class ProgramProcessor:
         source files to the output directory.
     '''
 
-    def __init__(self, sourceFile):
-        self.rootFile = sourceFile
-        self.rootName = pathlib.Path(sourceFile).stem
-        self.rootDir = os.path.dirname(sourceFile)
-        self.fileTree = {}
-        self.logTypeCount = 1
-        self.injectors = []
-        self.fileQueue = [sourceFile]
+    def __init__(self, sourceFile, workingDirectory):
+        self.sourceFile = sourceFile
+        self.sourceFileDirectory = os.path.dirname(sourceFile)
+        self.outputDirectory = os.path.join(workingDirectory, "output")
+        self.clearAndCreateFolder(self.outputDirectory)
 
-    def run(self, output_dir: str = "output"):
-        '''
-            Injects logs into files in the queue and add to file tree.
-            Any local imports found when processing a file is added to
-            the queue.
-        '''
-        while len(self.fileQueue) > 0: 
-            path = self.fileQueue.pop()
-            inj = LogInjector(path, self.logTypeCount, self.rootFile)
-            inj.run()
-            self.injectors.append(inj)
+    def run(self):
 
-            self.fileTree[inj.fileTreeKey] = {
-                "sst": inj.sst.tree,
-                "source": inj.source,
-            }
+        filePaths = findLocalImports(self.sourceFile)
 
-            self.addToQueue(inj.importsFound)
-            self.logTypeCount = inj.sst.logTypeId
+        for currFilePath in filePaths:
+            # Create output file path and output file directory
+            currRelPath = os.path.relpath(currFilePath, self.sourceFileDirectory)
+            outputFilePath = os.path.join(self.outputDirectory, currRelPath)
+            outputFileDir = os.path.dirname(outputFilePath)
 
-        # Create output folder if it doesn't exist
-        outputFolder = os.path.join(os.path.abspath(os.getcwd()), output_dir)
-        self.clearAndCreateFolder(outputFolder)
-                  
-        # Write each injected source to file
-        for inj in self.injectors:
-            currFolder = os.path.join(outputFolder, inj.relativeDir)
-            if not os.path.exists(currFolder):
-                os.makedirs(currFolder)
+            # Create directory if it doesn't exist
+            if (not os.path.exists(outputFileDir)):
+                os.makedirs(outputFileDir)
 
-            if (self.rootFile == inj.sourceFile):
-                source = self.getInjectedSourceRoot(inj, inj.fileName)
-            else:
-                source = self.getInjectedSource(inj)
+            with open(currFilePath, 'r') as f:
+                source = f.read()
 
-            filePath = os.path.join(currFolder, inj.fileNameWExtension)
-            with open(filePath, "w+") as f:
+            with open(outputFilePath, 'w+') as f:
                 f.write(source)
-
-    def addToQueue(self, paths):
-        '''
-            Adds the given paths to the queue if it is not already in the queue
-            or if it hasn't already been processed.
-        '''
-        for currentImport in paths:
-            if currentImport not in self.fileQueue and currentImport not in self.fileTree:
-                self.fileQueue.append(currentImport)     
-            
-
-    def getInjectedSource(self, inj):
-        '''
-            Given an injector object, this funtion returns 
-            a program injected with diagnostic logs.
-        '''
-        loggingSetupNodes = helper.getLoggingSetup()
-        injectedSourceNodes = inj.injectedTree.body
-
-        injectedNodes = loggingSetupNodes.body + injectedSourceNodes
-        return ast.unparse(injectedNodes)
-
-    def getInjectedSourceRoot(self, inj, fileName):
-        '''
-            Given an injector object, this funtion returns 
-            a program injected with diagnostic logs.
-
-            Args:
-                inj: Injector object containing the processed AST
-                fileName: Name for the logger setup
-            
-            Returns:
-                str: The injected source code as a string
-        '''
-        loggingSetupNodes = helper.getRootLoggingSetup(str(fileName))
-        fileTreeNodes = [
-            helper.getLoggingStatement(
-                json.dumps(self.fileTree))
-        ]
-        injectedSourceNodes = inj.injectedTree.body
-
-        injectedNodes = loggingSetupNodes.body + fileTreeNodes + injectedSourceNodes
-        return ast.unparse(injectedNodes)
     
     def clearAndCreateFolder (self, path):
         '''
