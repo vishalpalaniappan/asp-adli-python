fd61b2d399c0908687d78fa39fec936bcb201e66
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-02-22 23:25:55 -0500

    Refactor getDisabledVariables and improve comments.

--- injector/CheckDisabledVariable.py
+++ injector/CheckDisabledVariable.py
@@ -1,14 +1,23 @@
 import re, ast
 
 def getDisabledVariables(node):
+    '''
+        This function parses the variable disable comments and returns
+        a list with the disabled variables. 
 
+        It parses triple quote comments. 
+        Example:
+        \'\'\'adli-variable-disable value variables\'\'\'
+    '''
+    
+    disabledVariables = []
     if "value" in node._fields and isinstance(node.value, ast.Constant):
 
         value = node.value.value
         variables = re.findall("adli-disable-variable (.*)", value)
 
         if len(variables) > 0:
-            return variables[0].split()
+            disabledVariables = variables[0].split()
         
-    return []
+    return disabledVariables
         
