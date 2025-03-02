853a2e9cb97f3a95744a827e23ce1abf962134a8
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-03-02 18:04:38 -0500

    Add getDisabledVariables to helper functions.

--- injector/helper.py
+++ injector/helper.py
@@ -2,6 +2,7 @@ import ast
 import os
 import copy
 import json
+import re
 
 def checkImport(rootDir, node):
     """
@@ -188,3 +189,25 @@ def injectLoggingSetup(tree):
         loggingFunction.body,
         tree.body
     ], type_ignores=[])
+
+
+def getDisabledVariables(node):
+    """
+        This function parses the variable disable comments and returns
+        a list with the disabled variables. 
+
+        It parses triple quote comments. 
+        Example:
+        '''adli-variable-disable value variables'''
+    """
+    
+    disabledVariables = []
+    if "value" in node._fields and isinstance(node.value, ast.Constant):
+
+        value = node.value.value
+        variables = re.findall("adli-disable-variable (.*)", value)
+
+        if len(variables) > 0:
+            disabledVariables = variables[0].split()
+        
+    return disabledVariables
