21fde248c4239a0184e549269ebddbb7a67aadc9
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-03-29 22:57:50 -0400

    Update CollectVariableDefault documentation.

--- injector/VariableCollectors/CollectVariableDefault.py
+++ injector/VariableCollectors/CollectVariableDefault.py
@@ -4,11 +4,16 @@ from injector.helper import getEmptyRootNode
 
 class CollectVariableDefault(ast.NodeVisitor, VariableCollectorBase):
     '''
-        This class collects any name nodes in the provided node. For
+        This class collects any variables being written to. For
         nodes with a body, it removes the children before finding 
         the valid names. For example, in the case of a for loop,
         this would log the variable info for the current element in
         the loop.
+
+        Example:
+        for i in range(5):
+            
+        In this example, the variable i is logged.
     '''
 
     def __init__(self, node, logTypeId, funcId):
