1732516f1e8b9f5364080f13c08be2c0b6efecec
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-04-08 15:16:50 -0400

    Remove old code from adli_system and create initial functions for cloning repo.

--- adli_system.py
+++ adli_system.py
@@ -1,71 +1,30 @@
 import sys
-import json
 import argparse
-import subprocess
-import uuid
 
-def run(sys_def_file_path):
+def cloneRepo(url):
     '''
-        This program parses the system definition file and 
-        injects logs into every program in the system along
-        with a unique id.
+        Clone the given repo and load the system definition file.
     '''
-
-    # Load the system definition file using the given path
-    try:
-        with open(sys_def_file_path) as f:
-            sys_def_file = json.load(f)
-    except FileNotFoundError:
-        print(f"Error: System definition file '{sys_def_file_path}' not found.")
-        return -1
-    except json.JSONDecodeError:
-        print(f"Error: '{sys_def_file_path}' contains invalid JSON.")
-        return -1
-
-    '''
-    Create a unique instance id that is passed to every program in the system.
-    The system definition file contains systemId and systemVersion which allow us
-    to identify the system. This unique id will be used to identify every deployment
-    of the system.
-    '''
-    instance_uid = str(uuid.uuid4())
-
-    for path in sys_def_file["programs"]:
-        output = subprocess.run(
-            [
-                "python3",
-                "adli.py",
-                "-source",
-                path,
-                "-sysinfo",
-                sys_def_file_path,
-                "-uid",
-                instance_uid
-            ],
-            check=True,
-            capture_output=True,
-            text=True
-        )
-        print(f"Successfully processed {path}")
-        
-    return 0
 
 def main(argv):
 
     args_parser = argparse.ArgumentParser(
-        description="Injects diagnostic logs into multiple programs with the same system id."
+        description="This program injects diagnostic logs into all programs in the system given a repository URL." \
+        "The repo must have a system definition file."
     )
 
     args_parser.add_argument(
-        "adli_system_paths",
+        "repo_url",
         type=str,
-        help="Path to the system definition file."
+        help="URL of the repository containing the system to inject logs into."
     )
     
     parsed_args = args_parser.parse_args(argv[1:])
-    sys_def_file_path = parsed_args.adli_system_paths
+    url = parsed_args.repo_url
 
-    return run(sys_def_file_path)
+    cloneRepo(url)
+
+    return 1
 
 
 if __name__ == "__main__":
