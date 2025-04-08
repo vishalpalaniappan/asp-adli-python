425c16f36dc0714bd7d9d8a512968b70188f060a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-04-08 17:06:28 -0400

    Clone repo to temporary directory.

--- adli_system.py
+++ adli_system.py
@@ -1,10 +1,28 @@
 import sys
+import os
+import shutil
 import argparse
+import subprocess
+
+TEMP_DIRECTORY = "./temp"
 
 def cloneRepo(url):
     '''
         Clone the given repo and load the system definition file.
     '''
+
+    # Clear temporary directory
+    if os.path.exists(TEMP_DIRECTORY):
+        shutil.rmtree(TEMP_DIRECTORY)
+    os.makedirs(TEMP_DIRECTORY)
+    
+    # Clone the repo into the temporary directory
+    result = subprocess.run([
+        'git',
+        'clone',
+        url,
+        TEMP_DIRECTORY
+    ])
 
 def main(argv):
 
@@ -16,7 +34,7 @@ def main(argv):
     args_parser.add_argument(
         "repo_url",
         type=str,
-        help="URL of the repository containing the system to inject logs into."
+        help="URL of the repository containing the system to inject logs into.",
     )
     
     parsed_args = args_parser.parse_args(argv[1:])
