d5258b01a8128a753c9260da5006fce6401e265c
Vishal Palaniappan <101469936+vishalpalaniappan@users.noreply.github.com>
2025-05-21 20:51:04 -0400

    feat(runtime)!: Switch to KV-IR for compression CDL files, convert all runtime logs to JSON syntax and and improve variable logging. (#66)

--- injector/LoggerInstance/AdliLogger.py
+++ injector/LoggerInstance/AdliLogger.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from clp_logging.handlers import CLPFileHandler
+from clp_logging.handlers import ClpKeyValuePairStreamHandler
 
 import traceback
 import json
@@ -11,7 +11,7 @@ import uuid
 ADLI_EXECUTION_ID = str(uuid.uuid4())
 
 path = Path(os.path.dirname(__file__)) / f"{ADLI_EXECUTION_ID}.clp.zst"
-clp_handler = CLPFileHandler(path)
+clp_handler = ClpKeyValuePairStreamHandler(open(path, "wb"))
 logger = logging.getLogger("adli")
 logger.setLevel(logging.INFO)
 logger.addHandler(clp_handler)
@@ -32,6 +32,32 @@ class AdliLogger:
         self.inputCount = 0
         self.outputCount = 0
 
+    def processLevel(self, o, k, depth, max_depth):
+        if isinstance(o, (str, int, float, bool)) or o is None:
+            return o
+        
+        # TODO: Improve identifying circular references so we can remove max depth check.
+        # obj_id = id(o)
+        # if obj_id in self.visited:
+        #     return "<Circular Reference Detected>"
+        # self.visited.add(obj_id)
+
+        if depth > max_depth:
+            return '<Max Depth Reached>'
+        
+        if isinstance(o, dict):
+            return {str(k): self.processLevel(v, str(k), depth + 1, max_depth) for (k, v) in o.items()}
+        elif isinstance(o, (list, tuple, set)):
+            return [self.processLevel(item, str(k), depth + 1, max_depth) for item in o]
+        elif hasattr(o, '__dict__'):
+            return {k: self.processLevel(v, str(k), depth + 1, max_depth) for (k, v) in vars(o).items()}
+        else:
+            return str(o)
+
+    def variableToJson(self, obj, max_depth=8):
+        self.visited = set()
+        return self.processLevel(obj, "", 0, max_depth)
+
     def logVariable(self, varid, value):
         '''
             Logs the given varid and value. It also checks to see if the variable
@@ -43,23 +69,40 @@ class AdliLogger:
         self.count += 1
         self.variableLogCount += 1
         try:
-            dumpedValue = json.dumps(value, default=lambda o: o.__dict__ )
-            logger.info(f"# {varid} {dumpedValue}")
-        except:
-            logger.info(f"# {varid} {value}")
+            # Try to serialize the variable
+            adliValue = self.variableToJson(value)
+            varObj = {
+                "type": "adli_variable",
+                "varid": varid,
+                "value": adliValue
+            }
+            logger.info(varObj)
+        except Exception as e:
+            # Fallback to string if serialization fails.
+            varObj = {
+                "type": "adli_variable",
+                "varid": varid,
+                "value": str(value),
+                "serialization_error": str(e)
+            }
+            logger.info(varObj)
 
         return self.decodeInput(value)
 
     def logStmt(self, stmtId):
         '''
             Logs the statement id. This corresponds to a statement in the source
-            code. For example: a = 1 is mapped to stmtId 4.
+            code. For example: a = 1 can be mapped to stmtId 4.
 
             :param int stmtId: A number representing the mapped statement index in ltMap.
         '''
         self.count += 1
         self.stmtLogCount += 1
-        logger.info(stmtId)
+        stmtObj = {
+            "type": "adli_execution",
+            "value": stmtId
+        }
+        logger.info(stmtObj)
 
     def logException(self):
         '''
@@ -67,7 +110,11 @@ class AdliLogger:
         '''
         self.count += 1
         self.exceptionLogCount += 1
-        logger.error(f"? {traceback.format_exc()}")
+        exceptionObj = {
+            "type": "adli_exception",
+            "value": traceback.format_exc()
+        }
+        logger.info(exceptionObj)
 
     def logHeader(self, header):
         '''
@@ -77,17 +124,19 @@ class AdliLogger:
         '''
         self.count += 1
 
-        # Add execution infomration to header
+        # Add execution information to header
         header["execInfo"] = {
             "programExecutionId": ADLI_EXECUTION_ID,
             "timestamp": str(time.time()),
         }
 
+        # TODO: Debug why logging the header directly causes
+        # an error.
         logInfo = {
             "type": "adli_header",
-            "header": header
+            "header": json.dumps(header)
         }
-        logger.info(json.dumps(logInfo))
+        logger.info(logInfo)
 
     def encodeOutput(self, variableName, value):
         '''
@@ -107,7 +156,7 @@ class AdliLogger:
             "adliValue": value
         }
         
-        logger.info(json.dumps(logInfo))
+        logger.info(logInfo)
 
         return {
             "adliExecutionId": ADLI_EXECUTION_ID,
@@ -134,7 +183,7 @@ class AdliLogger:
                 "adliValue": value["adliValue"]
             }
 
-            logger.info(json.dumps(logInfo))
+            logger.info(logInfo)
 
             return value["adliValue"]
         
