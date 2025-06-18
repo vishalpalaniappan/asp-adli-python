99b523ea3ede38f1ccedf60d1133704fe8688a8a
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-06-18 07:26:03 -0400

    Improve inline injector functions.

--- injector/Injectors/InlineInjector.py
+++ injector/Injectors/InlineInjector.py
@@ -2,7 +2,7 @@ import ast
 from injector.VariableCollectors.CollectVariableDefault import CollectVariableDefault
 from injector.helper import getEmptyRootNode
 
-from injector.helper import getVarLogStmt, getLtLogStmt
+from injector.helper import getVarLogStmt, getLtLogStmt, getUniqueIdNamedAssignStmt, getLocalVarsLogStmt
 
 class InlineInjector(ast.NodeTransformer):
     
@@ -17,27 +17,29 @@ class InlineInjector(ast.NodeTransformer):
             node = getEmptyRootNode(node)
 
         self.generic_visit(node)
+        
 
     def visit_GeneratorExp(self, node):        
         variables = CollectVariableDefault(node, self.logTypeId, self.funcId).variables
         self.variables += variables
-
+        
         elts = [
+            getUniqueIdNamedAssignStmt(),
+            getLocalVarsLogStmt(),
+        ]
+
+        elts = elts + [
             getLtLogStmt(self.logTypeId),
             node.elt
         ]
 
-        for variable in variables:
-            stmt = getVarLogStmt(variable["syntax"], variable["varId"])
-            stmt = stmt.value
-            elts.append(stmt)
-
         node.elt = ast.Subscript(
             value=ast.Tuple(
                 elts=elts, ctx= ast.Load()),
-            slice= ast.Constant(1),
+            slice= ast.Constant(-1),
             ctx= ast.Load()
         )
+
         return node
     
     def visit_Lambda(self, node):    
@@ -45,19 +47,18 @@ class InlineInjector(ast.NodeTransformer):
         self.variables += variables
 
         elts = [
+            getUniqueIdNamedAssignStmt(),
+            getLocalVarsLogStmt(),
+        ]
+
+        elts = elts + [
             getLtLogStmt(self.logTypeId),
             node.body
         ]
 
-        for variable in variables:
-            stmt = getVarLogStmt(variable["syntax"], variable["varId"])
-            stmt = stmt.value
-            elts.append(stmt)
-
         node.body = ast.Subscript(
-            value=ast.Tuple(
-                elts=elts, ctx= ast.Load()),
-            slice= ast.Constant(1),
+            value=ast.Tuple(elts=elts, ctx= ast.Load()),
+            slice= ast.Constant(-1),
             ctx= ast.Load()
         )
         return node
