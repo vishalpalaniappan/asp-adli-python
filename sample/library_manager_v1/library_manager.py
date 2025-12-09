5274aab1bfd585960dc73f9270dc188619a3c9af
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-12-09 07:36:01 -0500

    Update implementation of library manager and update mapping.

--- sample/library_manager_v1/library_manager.py
+++ sample/library_manager_v1/library_manager.py
@@ -1,7 +1,14 @@
 import sys
 
 def place_book_on_shelf(book_shelf, name, genre):
-
+    '''
+    This function places the given book on the shelf 
+    in a slot determined by the first letter of its name.
+    
+    :param book_shelf: Bookshelf to place the book on.
+    :param name: Name of the book.
+    :param genre: Genre of the book.
+    '''
     print(f"Accepted book: {name} (Genre: {genre})")
     
     firstLetter = name[0].upper()
@@ -16,7 +23,9 @@ def place_book_on_shelf(book_shelf, name, genre):
     return book_shelf
 
 def accept_book():
-
+    '''
+    Accepts a book and genre from the user and returns it.
+    '''
     print("\nEnter book details:")
         
     name = input("Book name: ")
@@ -27,15 +36,19 @@ def accept_book():
 
 
 def library_manager():
-
+    '''
+    Runs the library manager to accept books until the user decides to stop.
+    '''
     book_shelf = {}
 
     while True:
-
         book_details = accept_book()
+        book_shelf = place_book_on_shelf(
+            book_shelf, 
+            book_details["name"], 
+            book_details["genre"]
+        )
 
-        book_shelf = place_book_on_shelf(book_shelf, book_details["name"], book_details["genre"])
-        
         more = input("Add another book? (y): ").lower()
         
         if more != "y":
