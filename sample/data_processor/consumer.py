318d8983106eec76b922868d26f5f6cf158a2119
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-01-07 13:35:45 -0500

    Simplify to make instrumentation easier and remove old variables.

--- sample/data_processor/consumer.py
+++ sample/data_processor/consumer.py
@@ -4,12 +4,8 @@ import queue
 class ConsumerThread(threading.Thread):
     def __init__(self, queue):
         super().__init__(daemon=True)
-        self.book_shelf = {}
         self.queue = queue
         self.start()
-
-    def __del__(self):
-        print(f"{threading.get_ident()} Terminating consumer thread")
 
     def run(self):
         while True:
--- sample/data_processor/worker.py
+++ sample/data_processor/worker.py
@@ -4,14 +4,10 @@ import queue
 class WorkerThread(threading.Thread):
     def __init__(self, id, queue, consumerQueue):
         super().__init__(daemon=True)
-        self.book_shelf = {}
         self.queue = queue
         self.id = id
         self.consumer_queue = consumerQueue
         self.start()
-
-    def __del__(self):
-        print(f"{threading.get_ident()} Terminating worker thread.")
 
     def run(self):
         while True:
