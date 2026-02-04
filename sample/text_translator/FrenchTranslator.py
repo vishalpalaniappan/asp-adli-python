33c1e0db83580a8cae2d8119d6ba5c18fb017bcf
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-04 03:43:43 -0500

    Restructure data_processor to text_translator.

---
+++ sample/text_translator/FrenchTranslator.py
@@ -0,0 +1,24 @@
+import threading
+import queue
+
+class FrenchTranslator(threading.Thread):
+    def __init__(self, id, queue, consumerQueue):
+        super().__init__(daemon=True)
+        self.queue = queue
+        self.id = id
+        self.consumer_queue = consumerQueue
+        self.start()
+
+    def run(self):
+        while True:
+            try:
+                msg = self.queue.get(timeout=10)
+            except queue.Empty:
+                continue
+
+            print("Worker Received message:", msg)
+            msg = msg[0] + msg
+            self.consumer_queue.put({
+                "id": self.id,
+                "data": msg
+            })
---
+++ sample/text_translator/text_translator.py
@@ -0,0 +1,29 @@
+import queue
+import sys
+import time
+from consumer import ConsumerThread
+from TamilTranslator import TamilTranslator
+from FrenchTranslator import FrenchTranslator
+from SpanishTranslator import SpanishTranslator
+
+def TextTranslator():
+    consumer_queue = queue.Queue()
+    ConsumerThread(consumer_queue)
+
+    message_queue_spanish = queue.Queue()
+    message_queue_french = queue.Queue()
+    message_queue_tamil = queue.Queue()
+
+    SpanishTranslator(1, message_queue_spanish, consumer_queue)
+    FrenchTranslator(2, message_queue_french, consumer_queue)
+    TamilTranslator(3, message_queue_tamil, consumer_queue)
+
+    while True:    
+        data = input("\nEnter any string:")
+        message_queue_spanish.put(data)
+        message_queue_french.put(data)
+        message_queue_tamil.put(data)
+        time.sleep(1)
+
+if "__main__" == __name__:
+    sys.exit(TextTranslator())
